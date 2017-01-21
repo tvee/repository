1ec2640fac7ec25360a33923465704a332cf7874
tvee <koditvzenders@outlook.com>
2017-01-21 22:56:05 +0100

    sebn

--- plugin.video.TVEE/sebn.py
+++ plugin.video.TVEE/sebn.py
@@ -28,6 +28,12 @@ def BVLSMain():
     match = re.compile('<div id="day">.*?h1>(.*?)</h1>.*?h1>(.*?)</h1>', re.IGNORECASE | re.DOTALL).findall(listhtml)
     for text1, text2 in match:
         BVLSaddDir('[COLOR cornflowerblue][B]' + text1 + ' [/B][/COLOR]' + text2,'',66,'http://sebn.sc/images/logo.png', Folder=False) 
+    BVLSaddDir('Maandag','http://sebn.sc/',67,'http://sebn.sc/images/logo.png')
+    BVLSaddDir('Dinsdag','http://sebn.sc/',68,'http://sebn.sc/images/logo.png')
+    BVLSaddDir('Woensdag','http://sebn.sc/',69,'http://sebn.sc/images/logo.png')
+    BVLSaddDir('Donderdag','http://sebn.sc/',70,'http://sebn.sc/images/logo.png')
+    BVLSaddDir('Vrijdag','http://sebn.sc/',71,'http://sebn.sc/images/logo.png')
+    BVLSaddDir('Zaterdag','http://sebn.sc/',72,'http://sebn.sc/images/logo.png')
     BVLSaddDir('Zondag','http://sebn.sc/',73,'http://sebn.sc/images/logo.png')
     BVLSaddDir('Losse streams','http://sebn.sc/',54,'http://sebn.sc/images/logo.png')
     xbmcplugin.endOfDirectory(int(sys.argv[1]))
